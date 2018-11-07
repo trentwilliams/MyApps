6a990d2fb07c3afe3842bc95f97ac4b3b2ab6337
92019562 <92019562@LRN-WN-V-RDSP01.learn.cpp.ac.nz>
2018-11-07 21:38:38 +1300

    first sync

---
+++ src/myapps/MyApps.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myapps;
+
+/**
+ *
+ * @author 92019562
+ */
+public class MyApps {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
